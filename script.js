55b267e8e09ea3264fdd19042efe5ab77745d50f
ARBAZP <117530037+ARBAZP@users.noreply.github.com>
2023-03-27 22:33:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Your Script here.
 
-const lookup = {
+var lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
   'E': 'R','F': 'S','G': 'T','H': 'U',
   'I': 'V','J': 'W','K': 'X','L': 'Y',
@@ -13,9 +13,20 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	decodedArr=encodedStr.split(" ");
+	var decryptedArr=[];
+	for(let i=0;i<decodedArr.length;i++){
+		var word=decodedArr[i];	
+		var decryptedWord="";
+		for(let j=0;j<word.length;j++){		
+			decryptedWord+=lookup[word.charAt(j)];
+		}
+		decryptedArr.push(decryptedWord);
+	}
 
-  return ;//return decodedArr
+  return decryptedArr.join(" ");//return decodedArr
 }
+
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
